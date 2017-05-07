a3ce8ed8483314b7105bfd4460d811cad2fca768
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2017-05-07 19:35:24 +0300

    added missed comments

--- src/app/services/dom.service.ts
+++ src/app/services/dom.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DomService {
+  /**
+   * Creates error mark-up
+   * @param {String} message - error message
+   * @param {String} url - url with missing video
+   * @returns {string}
+   */
   static createErrorTemplate (message, url) {
     return `
         <div class="player-error">
@@ -13,12 +19,25 @@ export class DomService {
         </div>`;
   }
 
+  /**
+   * Replace inner HTML of the target element with error
+   * @param {Object} cfg - {
+   *  selector: '.some-css-class',
+   *  el: HTMLElement, // target element to replace its inner HTML
+   *  errorMessage: 'Error',
+   *  videoUrl: 'http://some.url/some-video'
+   * }
+   */
   static placeError (cfg: any) {
     const target = cfg.el.querySelector(cfg.selector);
 
     target.innerHTML = DomService.createErrorTemplate(cfg.errorMessage, cfg.videoUrl);
   }
 
+  /**
+   * Removes the loader element of certain element
+   * @param {HTMLElement} el - element to replace loader
+   */
   static removeLoader (el: any) {
     const loader = el.getElementsByClassName('fa-spin')[0];
 
