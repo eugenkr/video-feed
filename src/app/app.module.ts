d3c36e7a30c6c734937a703c5bc1a1a57df2d45f
Yevgen Kruglyk <ykruglyk@softserveinc.com>
2017-05-07 08:22:41 +0300

    added improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,9 @@ import { VideoService } from './services/video.service';
 import { PlayerService } from './services/player.service';
 import { DomService } from './services/dom.service';
 
-import { YoutubeVideoComponent } from './components/youtube-video/youtube-video.component';
-import { FacebookVideoComponent } from './components/facebook-video/facebook-video.component';
-import { UrlVideoComponent } from './components/url-video/url-video.component';
+import { YoutubeVideoComponent } from './components/youtube-video';
+import { FacebookVideoComponent } from './components/facebook-video';
+import { UrlVideoComponent } from './components/url-video';
 
 @NgModule({
   declarations: [
